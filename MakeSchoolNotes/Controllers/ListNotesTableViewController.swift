0528a09504c19a1c21bfde8dec0622a3c33bd485
RobertWais <wais.robert@uwlax.edu>
2018-07-05 08:47:43 -0700

    move untracked files

---
+++ MakeSchoolNotes/Controllers/ListNotesTableViewController.swift
@@ -0,0 +1,71 @@
+//
+//  ListNotesTableViewController.swift
+//  MakeSchoolNotes
+//
+//  Created by Chris Orcutt on 1/10/16.
+//  Copyright © 2016 MakeSchool. All rights reserved.
+//
+
+import UIKit
+
+class ListNotesTableViewController: UITableViewController {
+    
+    var notes = [Note](){
+        
+        //didSet is called if property changes 
+        didSet {
+            tableView.reloadData()
+        }
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // 1
+        return notes.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // 2
+        let cell = tableView.dequeueReusableCell(withIdentifier: "listNotesTableViewCell", for: indexPath) as! ListNotesTableViewCell
+       
+        let note = notes[indexPath.row]
+        cell.noteTitleLabel.text = note.title
+        cell.noteModificationTimeLabel.text = note.modificationTime.convertToString()
+        return cell
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        guard let identifier = segue.identifier else { return }
+        
+        switch identifier {
+        case "displayNote":
+            
+            guard let indexPath = tableView.indexPathForSelectedRow else {return}
+            let note = notes[indexPath.row]
+            let destination = segue.destination as! DisplayNoteViewController
+            destination.note = note
+            print("note cell tapped")
+            
+        case "addNote":
+            print("create note bar button item tapped")
+            
+        default:
+            print("unexpected segue identifier")
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            notes.remove(at: indexPath.row)
+        }
+    }
+    
+    @IBAction func unwindWithSegue(_ segue: UIStoryboardSegue) {
+        
+    }
+    
+}
---
+++ MakeSchoolNotes/Models/Notes.swift
@@ -0,0 +1,18 @@
+//
+//  Notes.swift
+//  MakeSchoolNotes
+//
+//  Created by Robert Wais on 7/4/18.
+//  Copyright © 2018 MakeSchool. All rights reserved.
+//
+
+import Foundation
+
+class Note {
+    
+    // MARK: - Properties
+    
+    var title = ""
+    var content = ""
+    var modificationTime = Date()
+}
